cc241c41111511bcd9533fde9c8d24d3349a3df7
Abhay Tank <iabhaytank@gmail.com>
2024-05-04 12:03:01 +0530

    basic folder structure

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import Nav from './components/Nav';
+import Body from './components/Body';
+import Footer from './components/Footer';
+
+export default function App() {
+    return (
+        <>
+            <Nav/>
+            <Body/>
+            <Footer/>
+        </>
+    )
+}
